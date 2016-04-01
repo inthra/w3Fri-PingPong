130cbff22122e7659670f794d1860c3202df56e6
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-04-01 09:24:29 -0700

    add jquery file, modified js scripts that basiclly work

--- js/scripts.js
+++ js/scripts.js
@@ -14,24 +14,31 @@ var divisible5 = function(number) {
     return false;
   }
 };
-
-var divisible15 = function(number) {
-  if (number % 15 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
 // End Business Logic Section
 
 // Start Interface Logic Section
 $(document).ready(function(){
-  $("#blank form").submit(function(event){
+  $("#blank").submit(function(event){
     event.preventDefault();
 
-    var rawNum = $("input#inputNum").val();
+    var rawNum = parseInt($("input#inputNum").val());
+    console.log(rawNum);
 
-    for (i = 1)
-  })
-}):
+    for (i = 1; i <= rawNum; i += 1) {
+console.log(i);
+      if (divisible3(i) && divisible5(i)) {
+        $("#answer").append("<li>" + "pingpong" + "</li>");
+      }
+      else if (divisible5(i)) {
+        $("#answer").append("<li>" + "pong" + "</li>");
+      }
+      else if (divisible3(i)) {
+        $("#answer").append("<li>" + "ping" + "</li>");
+      }
+      else {
+        $("#answer").append("<li>" + i + "</li>");
+      }
+    };
+  });
+});
 // End Business Logic Section
